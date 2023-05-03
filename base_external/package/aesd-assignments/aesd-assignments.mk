45385ba6f28b0183c58ec6c10e7e05390d5bb13b
Gian Claudio Koeppel <gckoeppel@gmail.com>
2023-05-03 21:52:15 +0200

    update package git version

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 85967f6d726d3fbe57498e2df82256bef6c0af3d
+AESD_ASSIGNMENTS_VERSION = a093c776e174ce0af5044f5a452b9b3290ccf8d6
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
